c1a1132912cc0a93957d21334fb357740a5af389
abu-falcon <nopufa89@gmail.com>
2022-02-15 23:17:39 +0400

    formatting

--- VTT.py
+++ VTT.py
@@ -1,33 +1,32 @@
-#Developed and coded by Momalekiii
-#Used libraries in this code are <--SpeechRecognition 3.8.1 & MoviePy 1.0.3-->
-#follow me on all Social media as : @momalekiii
+# Developed and coded by Momalekiii
+# Used libraries in this code are <--SpeechRecognition 3.8.1 & MoviePy 1.0.3-->
+# follow me on all Social media as : @momalekiii
 
 
-#Create a folder and put your video file and VTT.py in it then:
+# Create a folder and put your video file and VTT.py in it then:
 
-#First you need to import the libraries:
+# First you need to import the libraries:
 import sys
 
-import speech_recognition as sr
 import moviepy.editor as mp
-
+import speech_recognition as sr
 
 AUDIO_FILE = "converted.wav"
-RESULT_FILE = 'recognized.txt'
+RESULT_FILE = "recognized.txt"
 
-#handling the video file""
+# handling the video file""
 try:
-    movie_file = sys.argv[1] # % python VTT.py movie_file
-except IndexError: # no file was provided in the terminal
+    movie_file = sys.argv[1]  # % python VTT.py movie_file
+except IndexError:  # no file was provided in the terminal
     movie_file = input("Put the name of your video with the extention: ").strip()
 
-#Video to audio conversion : 
+# Video to audio conversion :
 clip = mp.VideoFileClip(movie_file)
 clip.audio.write_audiofile(AUDIO_FILE)
 
 
-#Speech recognition :
-recognizer = sr.Recognizer() 
+# Speech recognition :
+recognizer = sr.Recognizer()
 
 with sr.AudioFile(AUDIO_FILE) as source:
     audio_data = recognizer.record(source)
@@ -35,11 +34,9 @@ with sr.AudioFile(AUDIO_FILE) as source:
 text_result = recognizer.recognize_google(audio_data)
 
 
-#Exporting the results : 
-with open(RESULT_FILE,mode ='w') as file: 
-   file.write("Recognized Speech:") 
-   file.write("\n") 
-   file.write(text_result) 
-   print("Done!")
-   print("I added stuff here to see if I can pull request on my own repo")
-
+# Exporting the results :
+with open(RESULT_FILE, mode="w") as file:
+    file.write("Recognized Speech:")
+    file.write("\n")
+    file.write(text_result)
+    print("Done!")
